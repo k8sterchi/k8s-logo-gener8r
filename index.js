d2f786383039c8c8d1e45dca7bf6a288884f8be9
k8 <katesterchi@gmail.com>
2023-08-23 07:18:21 -0500

    added console logs to try to figure out why the generated svg file is blank

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const path = require('path');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
 async function main() {
+    console.log('Starting logo generation...');
     // Get user input using logoGenerator.getUserInput()
     const userInput = await logoGenerator.getUserInput();
+    console.log('User input:', userInput); // Print user input
 
     // Generate SVG code based on user input
     let svgCode = '';
 
     if (userInput.shape === 'circle') {
+        console.log('Generating circle SVG code...');
         const circle = new Circle(100, userInput.color);
         svgCode = circle.render();
     } else if (userInput.shape === 'triangle') {
+        console.log('Generating triangle SVG code...');
         const triangle = new Triangle(488, 182, userInput.color);
         svgCode = triangle.render();
     } else if (userInput.shape === 'square') {
+        console.log('Generating square SVG code...'); 
         const square = new Square(200, userInput.color);
         svgCode = square.render();
     }
