521d2c2ffa9e228bd9009f825508200f494bf802
k8 <katesterchi@gmail.com>
2023-08-23 07:04:34 -0500

    used the node.js built-in path.join method to direct the generated file to populate the example folder

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const logoGenerator = require('./lib/logoGenerator'); 
+const logoGenerator = require('./lib/logoGenerator');
+const fs = require('fs');
+const path = require('path');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
-// using async await instead of .then, as taught by tutor
 async function main() {
-  // here i will call functions from logoGenerator and other modules to execute application functionality
-  const userInput = await logoGenerator.getUserInput();
+    // Get user input using logoGenerator.getUserInput()
+    const userInput = await logoGenerator.getUserInput();
 
-  // Perform SVG generation and other logic based on the user input
-  // Call functions to generate the SVG logo with the provided input
-  // generate SVG file... 
+    // Generate SVG code based on user input
+    let svgCode = '';
+
+    if (userInput.shape === 'circle') {
+        const circle = new Circle(100, userInput.color);
+        svgCode = circle.render();
+    } else if (userInput.shape === 'triangle') {
+        const triangle = new Triangle(488, 182, userInput.color);
+        svgCode = triangle.render();
+    } else if (userInput.shape === 'square') {
+        const square = new Square(200, userInput.color);
+        svgCode = square.render();
+    }
+
+    // Specify the path to the "examples" folder
+    const examplesFolderPath = path.join(__dirname, 'examples');
+
+    // Ensure the "examples" folder exists
+    if (!fs.existsSync(examplesFolderPath)) {
+        fs.mkdirSync(examplesFolderPath);
+    }
+
+    // Write the generated SVG code to an SVG file in the "examples" folder
+    const svgFilePath = path.join(examplesFolderPath, 'generated-logo.svg');
+    fs.writeFileSync(svgFilePath, svgCode);
+
+    console.log(`SVG file generated and saved as "${svgFilePath}"`);
 }
 
 main();
