bfa7a0c593133997a8267d04f3d6f6f96d6d755b
k8 <katesterchi@gmail.com>
2023-08-23 21:39:41 -0600

    added to the READMe, changed the generated file name, corrected color to specify background in svgcode for shape, created svgText, added version to svg wrapper

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ a Node.js command-line application that takes in user input to generate a logo a
 - [Resources](#resources)
 - [License](#license)
 ## Installation
+1. Make sure Node.js is installed on your system. 
+
+2. Download or clone the code from my Github repository.
+
+3. Navigate to the project directory and install dependencies `npm install`
+
+4. After dependencies are installed, you can run the application from the terminal using the following command: `node index.js`
+
+5. Answer the questions prompted by the application. 
+
+6. Once you have completed the prompts, the application will generate a .svg file containing your logo. You can find the file within the 'examples' directory. 
+
 ## User Story
 >AS a freelance web developer
 
@@ -49,10 +61,16 @@ THEN I am shown a 300x200 pixel image that matches the criteria I entered
 ## Preview
 ## Demo
 ## Resources
+I pair programmed with my friend Dirt to figure out why my .svg file was being generated without any code based off the user inputs.
+
+MDN and ChatGPT continue to be helpful resources. 
+
+[For the license badge, I copied from this repo.](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba) But i changed the color to bright-green because it is my favorite color.
 ## License
-![License](https://img.shields.io/badge/License-MIT%20License-brightgreen)  
-[License](./LICENSE)
-This application is covered under the [MIT License](./LICENSE) license.
+This application is covered under the 
+[![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
+
+[MIT License](./LICENSE) license.
 
 
 
--- index.js
+++ index.js
@@ -4,28 +4,24 @@ const path = require('path');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
 async function main() {
-    console.log('Starting logo generation...');
     // Get user input using logoGenerator.getUserInput()
     const userInput = await logoGenerator.getUserInput();
-    console.log('User input:', userInput); // Print user input
 
     // Generate SVG code based on user input
     let svgCode = '';
-
+console.log(userInput);
     if (userInput.shape === 'circle') {
-        console.log('Generating circle SVG code...');
-        const circle = new Circle(100, userInput.color);
+        const circle = new Circle(100, userInput.backgroundColor);
         svgCode = circle.render();
     } else if (userInput.shape === 'triangle') {
-        console.log('Generating triangle SVG code...');
-        const triangle = new Triangle(488, 182, userInput.color);
+        const triangle = new Triangle(488, 182, userInput.backgroundColor);
         svgCode = triangle.render();
     } else if (userInput.shape === 'square') {
-        console.log('Generating square SVG code...'); 
-        const square = new Square(200, userInput.color);
+        const square = new Square(200, userInput.backgroundColor);
         svgCode = square.render();
     }
 
+const svgText = `<text x="60" y="110" font-size="50px" fill=${userInput.textColor}> ${userInput.text}</text>`
     // Specify the path to the "examples" folder
     const examplesFolderPath = path.join(__dirname, 'examples');
 
@@ -33,12 +29,16 @@ async function main() {
     if (!fs.existsSync(examplesFolderPath)) {
         fs.mkdirSync(examplesFolderPath);
     }
+console.log(svgCode, 'this should be the code')
+    // SVG wrapper with specified width and height
+    const svgWrapper = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${svgCode}${svgText}</svg>`;
 
     // Write the generated SVG code to an SVG file in the "examples" folder
-    const svgFilePath = path.join(examplesFolderPath, 'generated-logo.svg');
-    fs.writeFileSync(svgFilePath, svgCode);
+    const svgFilePath = path.join(examplesFolderPath, 'logo.svg');
+    fs.writeFileSync(svgFilePath, svgWrapper);
 
     console.log(`SVG file generated and saved as "${svgFilePath}"`);
+    
 }
 
 main();
